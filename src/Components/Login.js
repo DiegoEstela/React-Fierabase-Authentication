df86f398a1ace6e2f75f0e6a15013296a7f1b9b7
Diego Ezequiel Estela <die.estela@gmail.com>
2021-09-08 13:24:41 -0300

    se realizo maqueta

---
+++ src/Components/Login.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+  const [error, setError] = useState(null);
+  return (
+    <div className="card">
+      <div className="card-header">
+        {error && <p className="error">{{ error }}</p>}
+        <h1>Log In</h1>
+      </div>
+      <div className="card-body">
+        <form>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="submit" value="Log In" />
+        </form>
+        <p>
+          Todavia no tenes usuario? <Link to="/signup">Sign Up</Link>{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
